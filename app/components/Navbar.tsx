ded9f38e8a464ae3cb88a42d3aa50c6a86f67f80
aditi <shand.aditi@gmail.com>
2026-04-18 02:17:21 -0700

    feat: add basic navbar with primary routes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
-    <div>
-      Navbar
+    <div className="flex gap-6 p-4 border-b">
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+      <Link href="/projects">Projects</Link>
+      <Link href="/leetcode">LeetCode</Link>
+      <Link href="/playground">Playground</Link>
     </div>
   );
 }
